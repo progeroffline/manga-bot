738201864151acded40916d8320ca70d4fb029ba
Niryall <marinalex2042gmail.com>
2024-05-13 17:16:01 +0300

    test_bot_tg

---
+++ app.py
@@ -0,0 +1,62 @@
+import asyncio
+from aiogram import Bot, Dispatcher, html
+from aiogram.client.default import DefaultBotProperties
+from aiogram.enums import ParseMode
+from aiogram.filters import CommandStart
+from aiogram.types import Message
+
+from environs import Env
+
+env = Env()
+env.read_env()
+
+TELEGRAM_BOT_TOKEN = env.str("TELEGRAM_BOT_TOKEN")
+ADMIN_ID = env.int("ADMIN_ID")
+# Initialize Bot instance with default bot properties which will be passed to all API calls
+bot = Bot(token=TELEGRAM_BOT_TOKEN,
+          default=DefaultBotProperties(parse_mode=ParseMode.HTML))
+
+# All handlers should be attached to the Router (or Dispatcher)
+dp = Dispatcher()
+
+
+@dp.message(CommandStart())
+async def command_start_handler(message: Message) -> None:
+    """
+    This handler receives messages with `/start` command
+    """
+
+    print(message)
+
+    await message.answer(f"Hello, @{message.from_user.username} {message.from_user.full_name}!")
+
+    if message.from_user.id == ADMIN_ID:
+        return None
+    if message.from_user.username == None:
+        await bot.send_message(chat_id=ADMIN_ID, text=f"{message.from_user.id}")
+    else:
+        await bot.send_message(chat_id=ADMIN_ID, text=f"@{message.from_user.username}")
+
+
+@dp.message()
+async def echo_handler(message: Message) -> None:
+    """
+    Handler will forward receive a message back to the sender
+
+    By default, message handler will handle all message types (like a text, photo, sticker etc.)
+    """
+    try:
+        # Send a copy of the received message
+        await message.send_copy(chat_id=message.chat.id)
+    except TypeError:
+        # But not all the types is supported to be copied so need to handle it
+        await message.answer("Nice try!")
+
+
+async def main() -> None:
+    # And the run events dispatching
+    await dp.start_polling(bot)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
