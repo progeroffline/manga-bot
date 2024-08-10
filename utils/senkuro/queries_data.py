04c54da5e250e769442e5e6ca2eb4b1dc7adc1cd
progeroffline <progeroffline@gmail.com>
2024-08-11 01:34:21 +0300

    Create default queries_data file

---
+++ utils/senkuro/queries_data.py
@@ -0,0 +1,75 @@
+home = {
+    "extensions": {
+        "persistedQuery": {
+            "sha256Hash": "a4da0008c5103bfe9ea914dfd51091a6a67924b9d417806196a9e077168cf663",
+            "version": 1,
+        },
+    },
+    "operationName": "fetchMainPage",
+    "variables": {
+        "genre": {
+            "exclude": [
+                "hentai",
+            ],
+        },
+    },
+}
+
+catalog = {
+    "extensions": {
+        "persistedQuery": {
+            "sha256Hash": "6547bf3c404812150e9e0429adba781f36a0ef17b12d4cb31d8caf1d8bac91e1",
+            "version": 1,
+        },
+    },
+    "operationName": "fetchMangas",
+    "variables": {
+        "after": None,
+        "bookmark": {
+            "exclude": [],
+            "include": [],
+        },
+        "chapters": {},
+        "format": {
+            "exclude": [],
+            "include": [],
+        },
+        "genre": {
+            "exclude": [
+                "hentai",
+            ],
+            "include": [],
+        },
+        "orderDirection": "DESC",
+        "orderField": "CREATED_AT",
+        "originCountry": {
+            "exclude": [],
+            "include": [],
+        },
+        "rating": {
+            "exclude": [],
+            "include": [],
+        },
+        "search": None,
+        "source": {
+            "exclude": [],
+            "include": [],
+        },
+        "status": {
+            "exclude": [],
+            "include": [],
+        },
+        "tag": {
+            "exclude": [],
+            "include": [],
+        },
+        "translitionStatus": {
+            "exclude": [],
+            "include": [],
+        },
+        "type": {
+            "exclude": [],
+            "include": [],
+        },
+    },
+}
