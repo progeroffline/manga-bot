69b0f790a0a2644e3425f11fd6319bf2a47b041b
progeroffline <progeroffline@gmail.com>
2024-08-11 01:33:58 +0300

    Create consts

---
+++ utils/senkuro/consts.py
@@ -0,0 +1,9 @@
+# Auth section
+headers = {
+    "User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:125.0) Gecko/20100101 Firefox/125.0",
+    "Content-Type": "application/json",
+}
+
+# Urls section
+api_graphql = "https://api.senkuro.com/graphql"
+home = "https://senkuro.com/manga/{slug}/chapters"
